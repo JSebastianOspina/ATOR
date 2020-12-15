7f13ddcc4ef78006d56c2232f878419928dd6486
Sebastian Ospina <sebastianospinacontacto@gmail.com>
2020-12-15 13:58:59 -0600

    Slider and menu

---
+++ components/Slider.vue
@@ -0,0 +1,29 @@
+<template>
+  <splide :options="options">
+    <splide-slide>
+      <img src="images01.jpeg">
+    </splide-slide>
+    <splide-slide>
+      <img src="images02.jpeg">
+    </splide-slide>
+    <splide-slide>
+      <img src="images01.jpeg">
+    </splide-slide>
+  </splide>
+</template>
+
+<script>
+import '@splidejs/splide/dist/css/themes/splide-sea-green.min.css';
+
+  export default { 
+    data() {
+      return {
+        options: {
+          rewind : true,
+          autoWidth : true,
+          gap    : '1rem',
+        },
+      };
+    },
+  }
+</script>
